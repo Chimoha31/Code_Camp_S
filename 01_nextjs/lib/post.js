3fe3dc1370418d6005cc1f27639a8becd3cf0df8
Chimoha31 <mokochii1108@gmail.com>
2022-12-06 20:23:48 -0800

    Installed matter

---
+++ 01_nextjs/lib/post.js
@@ -0,0 +1,21 @@
+// 取り出す為のpathが必要(directoryを取得することが可能)
+import path from "path";
+import fs from "fs";
+import matter from 'matter';
+
+// path.join()第一引数がcurrent directory(process.cdw()で表せる)。第二引数が取得してくるdirectory。
+const postsDirectory = path.join(process.cwd(), "posts");
+
+// mdファイルのデータを取り出す
+export function getPostsData() {
+  // ファイル名を取り出す
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // replace()第一引数に(/\$/, "")$の前に省きたい文字列を入れ、第二引数を空にすると$前に文字列を省いたものを格納する。
+    const id = fileNames.replace(/\.md$/, ""); //ファイル名
+
+    // ファイルの中身を取り出す
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+  });
+}
