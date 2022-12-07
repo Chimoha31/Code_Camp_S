4caf62d8ab9340a4ffae4cf3649376eb419f84ce
Chimoha31 <mokochii1108@gmail.com>
2022-12-06 18:19:39 -0800

    Added images

--- 01_nextjs/components/Layout.js
+++ 01_nextjs/components/Layout.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 const name = "Teddy Maekawa";
 export const siteTitle = "Next.js blog";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
-      <header>
-        <img src="/images/profile.jpg" />
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img src="/images/profile.jpg" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
 
       <main>{children}</main>
--- 01_nextjs/pages/index.js
+++ 01_nextjs/pages/index.js
@@ -3,7 +3,33 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I am one year old baby boy Teddy kun 🐶 I'm a cute puppy ever in the
+          world 🌍
+        </p>
+      </section>
+
+      <section>
+        <h2>📝 Engineer blog</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/html.jpeg" alt="" />
+            </Link>
+            <Link href="/">
+              <p>First Step: Let's learn HTML first!!</p>
+            </Link>
+            <br />
+            <small>December, 7, 2022</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
